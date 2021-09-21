92e995a837ee737f12f8a737850cd5a03f5388f6
Jesse <jessecharles@gmail.com>
2021-09-21 16:36:53 -0600

    first ui

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <v-app-bar dense flat fixed color="white" class= "mb-16">
+      <v-app-bar-title>
+        <span class = "font-weight-light">Dx</span>
+        <span class = "font-weight-bold">Stake</span>
+      </v-app-bar-title>
+        <v-spacer></v-spacer>
+        <span class = "font-weight-bold">Metamask Stuff</span>
+        <v-icon small>find metamask icon</v-icon>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="home">
+  <v-content class="my-16 mx-16">
+   <v-card
+    :loading="loading"
+    class="mx-auto my-12"
+    max-width="600"
+    max-height="800"
+  >
+  <v-card-title class= "justify-center">Insert cool logo here</v-card-title>
+  <div class="text-center mx-auto my-12">
+    <v-btn class="grey lighten-3 pink--text mx-3 justify-center">DXSTAKE</v-btn>
+  <v-btn class="grey lighten-3 pink--text mx-3">METRICS</v-btn>
+        </div>
+  <div class="text-center mx-auto my-5">
+    Wallet Balace = 420.69
+        </div>
+  
+      <v-form v-model="valid">
+      <v-container class ="justify-center">
+        <v-row>
+          <v-col
+            
+        
+          >
+          
+            <v-text-field class="ml-5 justify-center"
+              v-model="firstname"
+              :rules="nameRules"
+              label="0"
+              required
+            ></v-text-field>
+          </v-col>
+            <v-btn rounded class="teal white--text mx-9 my-9">STAKE</v-btn>
+        </v-row>
+        <v-row>
+
+          <v-col>
+            <v-text-field class="ml-5 justify-center"
+              v-model="lastname"
+              :rules="nameRules"
+              label="0"
+              required
+            ></v-text-field>
+          </v-col>
+   <v-btn rounded class="teal white--text mx-9 my-9">UNSTAKE</v-btn>
+        </v-row>
+      </v-container>
+    </v-form>
+    <div class="text-center mx-auto my-5">
+    Staking Rewards: 0
+    </div>
+        <div class="text-center mx-auto my-5">
+     <v-btn class="pink white--text mx-3 my-5 justify-center">REINVEST</v-btn>
+  <v-btn class="teal white--text mx-3 my-5 justify-center">REINVEST</v-btn>
+  <v-btn class="pink white--text mx-3 my-5 justify-center">REINVEST</v-btn>
+    </div>
+   </v-card>
+  </v-content>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+
   }
-}
 </script>
