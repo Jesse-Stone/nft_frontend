176fcdf14d6d820510156e1ca661bde1a5cf482f
Jesse <jessecharles@gmail.com>
2021-09-21 17:05:12 -0600

    color fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-app class="green darken-4">
+  <v-app>
     <Navbar />
-    <v-content>
+    <v-content class="primary">
       <router-view></router-view>
     </v-content>
   </v-app>
